1a24f408910b08c2b758cb9e32665aa7924fe7a6
Stanislav Targonskiy <super.12332@yandex.ru>
2023-08-07 17:41:19 +0300

    backend

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(requestLogger);
 
 app.use(cors);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 // Ошибки логгера
 app.use(errorLogger);
 
