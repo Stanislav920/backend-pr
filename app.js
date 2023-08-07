79a451c40ced85bf620a6b2ff95bf215226c5028
Stanislav Targonskiy <super.12332@yandex.ru>
2023-08-07 16:55:56 +0300

    Файл бекенд

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const router = require('./routes');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+const { centralizedHandler } = require('./middlewares/centralized-handler');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+// Логгер
+app.use(requestLogger);
+
+app.use(cors);
+
+// Ошибки логгера
+app.use(errorLogger);
+
+// Обрабочек ответа.
+app.use(errors());
+app.use(centralizedHandler);
+
+app.listen(PORT, () => console.log('Сервер запущен!'));
