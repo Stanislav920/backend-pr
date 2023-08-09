b1b5b12ff83e5780e83f6a3fd332e5da8ed61944
Stanislav Targonskiy <super.12332@yandex.ru>
2023-08-09 12:52:51 +0300

    backend

--- middlewares/centralized-handler.js
+++ middlewares/centralized-handler.js
@@ -1,4 +1,4 @@
-module.exports = ((err, req, res, next) => {
+export default ((err, req, res, next) => {
   const { responseStatus = err.status || 500, message } = err;
   res.status(responseStatus).send({ message: responseStatus === 500 ? 'На сервере произошла ошибка, свяжитесь с администратором' : message });
   next();
